9c3d7db13885dc2050361d12f94d5c55e59ed02a
Ymoka <contact@lucasengelmann.fr>
2025-01-09 14:43:05 +0100

    Task P1C2.3

--- P1/P1C2.3/Program.cs
+++ P1/P1C2.3/Program.cs
@@ -1,11 +1,14 @@
 ﻿// TODO, étape 1 :
 // Créer une variable nommée jourDeDepart avec pour valeur initiale 3
+int jourDeDepart = 3;
 
 // TODO, étape 2 :
 // Créer une constante nommée joursDansSemaine avec pour valeur 7
+const int joursDansSemaine = 7;
 
 // TODO, étape 3 :
 // Utiliser un opérateur de raccourci pour ajouter la valeur de la constante joursDansSemaine à la variable jourDeDepart
+jourDeDepart += joursDansSemaine;
 
 // Afficher le résultat 
 Console.WriteLine("Il y a " + joursDansSemaine + " jours dans la semaine.");
