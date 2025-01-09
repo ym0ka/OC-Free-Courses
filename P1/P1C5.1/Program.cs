c32cc506c2bc2b5ca12ef5832c49a389acf295e7
Ymoka <contact@lucasengelmann.fr>
2025-01-09 15:53:41 +0100

    Task P1C5.1

--- P1/P1C5.1/Program.cs
+++ P1/P1C5.1/Program.cs
@@ -1,11 +1,11 @@
 ﻿// TODO : Déclarer une variable nommée couleurs d'un array de string de longueur 5
-
+string[] couleurs = new String[5];
 
 // TODO : Remplir le tableau avec les couleurs demandées dans le README.md
-
+couleurs = ["rouge", "jaune", "orange", "vert", "bleu"];
 
 // TODO : Remplacer vert par émeraude dans le tableau
-
+couleurs[3] = "émeraude";
 
 // Afficher le contenu du tableau
 foreach (string couleur in couleurs)
