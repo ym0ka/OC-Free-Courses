9e74906752b25c3ddf928d305677cd088c415cba
Ymoka <contact@lucasengelmann.fr>
2025-01-09 16:11:54 +0100

    Task P1C5.3

--- P1/P1C5.3/Program.cs
+++ P1/P1C5.3/Program.cs
@@ -5,10 +5,10 @@ ingredients.Add("beurre");
 ingredients.Add("vanille");
 
 // TODO : ajouter un autre ingrédient à l'ensemble
-
+ingredients.Add("oeufs");
 
 // TODO : retirer la vanille de l'ensemble
-
+ingredients.Remove("vanille");
 
 // Afficher la liste des ingrédients
 Console.WriteLine("Voici la liste de nos ingrédients");
