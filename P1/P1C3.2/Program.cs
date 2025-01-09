1d6ae6a42cd56b03f0b0459c82d692051ff68139
Ymoka <contact@lucasengelmann.fr>
2025-01-09 14:56:52 +0100

    Task P1C3.2

--- P1/P1C3.2/Program.cs
+++ P1/P1C3.2/Program.cs
@@ -2,7 +2,7 @@
 string secondString = "chats";
 
 // TODO: créer un nouveau string animals disant 'j'aime les chiens et chats!'
-
+string animals = "j'aime les " + premierString + " et les " + secondString + "!";
 
 // Afficher le résultat
 Console.WriteLine(animals);
