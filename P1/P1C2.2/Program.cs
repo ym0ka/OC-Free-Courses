a51ba74bf78ac743d9a50700dc614f19a8ab2edd
Ymoka <contact@lucasengelmann.fr>
2025-01-09 14:35:25 +0100

    Task P1C2.2

--- P1/P1C2.2/Program.cs
+++ P1/P1C2.2/Program.cs
@@ -1,5 +1,5 @@
 ﻿int epargne = 3000;
 
 // TODO : modifier l'instruction suivante pour utiliser l'opérateur raccourci +=	
-epargne = epargne + 500;
+epargne += 500;
 Console.WriteLine("Le montant de votre épargne est de : " + epargne);
