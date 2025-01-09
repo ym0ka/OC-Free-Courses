6833762b3f1d5e8228dd0ec4f75aa31555f2e8f1
Ymoka <contact@lucasengelmann.fr>
2025-01-09 16:28:21 +0100

    Task P1C5.4

--- P1/P1C5.4/Program.cs
+++ P1/P1C5.4/Program.cs
@@ -1,15 +1,19 @@
 ﻿IDictionary<string, int> mois = new Dictionary<string, int>();
 
 // TODO : Remplacer les noms par des constantes de type chaine de caractères
-mois.Add("Juin", 6);
-mois.Add("Septembre", 9);
-mois.Add("Mars", 5);
+const string cleJuin = "Juin";
+const string cleSeptembre = "Septembre";
+const string cleMars = "Mars";
+
+mois.Add(cleJuin, 6);
+mois.Add(cleSeptembre, 9);
+mois.Add(cleMars, 5);
 
 // TODO : Corriger la valeur de Mars avec (3)
-
+mois[cleMars] = 3;
 
 // TODO : Retirer Juin
-
+mois.Remove(cleJuin);
 
 // Afficher le contenu du dictionnaire
 Console.WriteLine("La liste des mois est :");
