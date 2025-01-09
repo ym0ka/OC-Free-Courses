a2b08f5e6cc3e61d8eebf889eead0ce3bd70cea1
Ymoka <contact@lucasengelmann.fr>
2025-01-09 15:38:00 +0100

    Task P1C4 + added using keywoard to prevent CS0246 error (reference missing)

--- P1/P1C4/Program.cs
+++ P1/P1C4/Program.cs
@@ -1,7 +1,11 @@
-﻿// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
+﻿using P1C4;
 
+// TODO, étape 1 : instancier un objet de la classe Livre et affecter cet objet à une variable nommée monLivre
+Livre monLivre = new Livre();
 // TODO, étape 2 : affecter une valeur aux champs titre, auteur et nombre de pages de votre objet
-
+monLivre.Titre = "Les Légendaires : La Pierre de Jovénia";
+monLivre.Auteur = "Patrick Sorbal";
+monLivre.NombreDePages = 48;
 // Afficher le contenu des champs de cet objet 
 Console.WriteLine("Le titre du livre est " + monLivre.Titre);
 Console.WriteLine("Son auteur est " + monLivre.Auteur);
