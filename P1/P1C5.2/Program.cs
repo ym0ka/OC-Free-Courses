bdea66931e86635e126b830260293e8972ea4480
Ymoka <contact@lucasengelmann.fr>
2025-01-09 16:07:37 +0100

    Task P1C5.2

--- P1/P1C5.2/Program.cs
+++ P1/P1C5.2/Program.cs
@@ -1,18 +1,21 @@
 ﻿// TODO : Remplacer le ?? par le code approprié pour créer une liste de chaînes
 
-IList <??> invites = new ?? ();
+IList <string> invites = new List<string>();
 
 // TODO : Ajouter Joe, Martin et Marie à la liste.
-
+invites.Add("Joe");
+invites.Add("Martin");
+invites.Add("Marie");
 
 // TODO : Compléter l'instruction suivante en remplaçant le ?? pour afficher la taille de la liste
-Console.WriteLine(invites.??);
+Console.WriteLine(invites.Count);
 
 // TODO : Remplacer Martin par Jean dans la liste
-
+invites.RemoveAt(1);
+invites.Insert(1, "Jean");
 
 // TODO : Retirer Joe de la liste
-
+invites.RemoveAt(0);
 
 // Afficher le contenu de la liste
 Console.WriteLine("Les invités sont :");
