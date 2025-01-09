40c024de3b9ff880452e05c17208658f7cadb42c
Ymoka <contact@lucasengelmann.fr>
2025-01-09 14:53:26 +0100

    Task P1C3.1

--- P1/P1C3.1/Program.cs
+++ P1/P1C3.1/Program.cs
@@ -2,7 +2,7 @@
 int denominateur = 4;
 
 // TODO : modifier l'instruction ci-dessous pour que la réponse prenne une valeur décimale
-int reponse = numerateur / denominateur;
+double reponse = numerateur / (double) denominateur;
 
 // Afficher le résultat 
 Console.WriteLine(numerateur + " / " + denominateur + " = " + reponse);
