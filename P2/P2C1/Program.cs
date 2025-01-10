327ebeff1bfea39a343eef011c9a2134bedaa0d7
Ymoka <contact@lucasengelmann.fr>
2025-01-10 15:01:03 +0100

    Task P2C1

--- P2/P2C1/Program.cs
+++ P2/P2C1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+public class BonjourLeMonde
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Bonjour le monde !");
+        AfficheBonjour();
+    }
+
+    public static void AfficheBonjour()
+    {
+        Console.WriteLine("Bonjour le monde !");
+    }
+}
+
