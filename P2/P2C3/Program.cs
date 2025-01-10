c42066d76f7d943e357978cc63d225784626ce09
Ymoka <contact@lucasengelmann.fr>
2025-01-10 15:31:57 +0100

    Task P2C3, for loop

--- P2/P2C3/Program.cs
+++ P2/P2C3/Program.cs
@@ -1,1 +1,23 @@
 ﻿//TODO : Créer une boucle for et while qui affiche à 5 reprises la phrase, « Je m'amuse comme un fou ! ».
+using System;
+
+namespace Boucles
+{
+    
+    public class Boucles
+    {
+        public static void Main(string[] args)
+        {
+            AmusonsNous();
+        }
+
+        public static void AmusonsNous()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Je m'amuse comme un fou !");
+            }
+        }
+    }
+
+}
