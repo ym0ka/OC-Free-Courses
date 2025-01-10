35544e6ff5ac3a05bc8c13e22eae6580803c3235
Ymoka <contact@lucasengelmann.fr>
2025-01-10 15:15:59 +0100

    Task P2C2.2

--- P2/P2C2.2/Program.cs
+++ P2/P2C2.2/Program.cs
@@ -3,23 +3,44 @@
 Couleur couleurChoisie = Couleur.Vert;
 
 // TODO : Remplacer la séquence if/else par une instruction switch équivalente qui fonctionne selon la valeur de couleurChoisie
-if (couleurChoisie == Couleur.Bleu)
+// if (couleurChoisie == Couleur.Bleu)
+// {
+//     Console.WriteLine("La couleur est bleue");
+// }
+// else if (couleurChoisie == Couleur.Vert)
+// {
+//     Console.WriteLine("La couleur est verte");
+// }
+// else if (couleurChoisie == Couleur.Rouge)
+// {
+//     Console.WriteLine("La couleur est rouge");
+// }
+// else if (couleurChoisie == Couleur.Jaune)
+// {
+//     Console.WriteLine("La couleur est jaune");
+// }
+// else
+// {
+//     Console.WriteLine("Cette couleur n'est pas connue !");
+// }
+
+switch (couleurChoisie)
 {
-    Console.WriteLine("La couleur est bleue");
-}
-else if (couleurChoisie == Couleur.Vert)
-{
-    Console.WriteLine("La couleur est verte");
-}
-else if (couleurChoisie == Couleur.Rouge)
-{
-    Console.WriteLine("La couleur est rouge");
-}
-else if (couleurChoisie == Couleur.Jaune)
-{
-    Console.WriteLine("La couleur est jaune");
-}
-else
-{
-    Console.WriteLine("Cette couleur n'est pas connue !");
+    
+    case Couleur.Bleu:
+        Console.WriteLine("La couleur est bleue");
+        break;
+    case Couleur.Vert:
+        Console.WriteLine("La couleur est verte");
+        break;
+    case Couleur.Rouge:
+        Console.WriteLine("La couleur est rouge");
+        break;
+    case Couleur.Jaune:
+        Console.WriteLine("La couleur est jaune");
+        break;
+    default:
+        Console.WriteLine("Cette couleur n'est pas connue !");
+        break;
+        
 }
