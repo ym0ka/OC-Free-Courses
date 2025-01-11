fd7171b9d3a95ea8f8c907d1905ceb498cb2ad54
Ymoka <contact@lucasengelmann.fr>
2025-01-11 12:44:09 +0100

    Task P2C5.2

--- P2/P2C5.2/DonneeUtilisateur.cs
+++ P2/P2C5.2/DonneeUtilisateur.cs
@@ -1,30 +1,32 @@
 ﻿//TODO
 //Ajoutez les lignes manquantes à ce fichier pour pouvoir y accéder depuis une autre classe.
-
-/// <summary>
-/// Demande à l'utilisateur de saisir une URL
-/// </summary>
-/// <returns>Une représentation sous forme de chaîne de caractères de l'URL saisie par l'utilisateur</returns>.
-public static string DemanderUneUrl()
+public class DonneeUtilisateur
 {
-    Console.WriteLine("Veuillez saisir une URL valide");
-    string url = Console.ReadLine();
-    return url;
-}
-
-/// <summary>
-/// Vérifier le formatage d'une URL
-/// </summary>
-/// <param name="chaineUrl"></param>
-/// <returns>vrai si le format correspond a une URL, sinon faux</returns>
-private static bool URLValide(string chaineUrl)
-{
-    if (Uri.IsWellFormedUriString(chaineUrl, UriKind.Absolute))
+    /// <summary>
+    /// Demande à l'utilisateur de saisir une URL
+    /// </summary>
+    /// <returns>Une représentation sous forme de chaîne de caractères de l'URL saisie par l'utilisateur</returns>.
+    public static string DemanderUneUrl()
     {
-        return true;
+        Console.WriteLine("Veuillez saisir une URL valide");
+        string url = Console.ReadLine();
+        return url;
     }
-    else
+
+    /// <summary>
+    /// Vérifier le formatage d'une URL
+    /// </summary>
+    /// <param name="chaineUrl"></param>
+    /// <returns>vrai si le format correspond a une URL, sinon faux</returns>
+    private static bool URLValide(string chaineUrl)
     {
-        return false;
+        if (Uri.IsWellFormedUriString(chaineUrl, UriKind.Absolute))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
