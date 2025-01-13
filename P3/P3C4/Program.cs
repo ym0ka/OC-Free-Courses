d792caf51e5fd669fa9422683d1e20d68197e6a7
Ymoka <contact@lucasengelmann.fr>
2025-01-13 11:03:38 +0100

    Task P3C4

--- P3/P3C4/Program.cs
+++ P3/P3C4/Program.cs
@@ -1,7 +1,35 @@
-﻿// TODO : créer deux méthodes
+﻿namespace P3C4
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // TODO : exécuter les méthodes de test pour les deux méthodes de la classe Program.cs
+            Test.TestCinqPlusTreizeEgalDixHuit();
+            Test.TestQuatorzeMoinsHuitEgalSix();
+            
+            // TODO : afficher le resultat de ces deux méthodes ici
+            int a = 5;
+            int b = 7;
+            int c = Addition(a, b);
+            Console.WriteLine($"Somme de {a} + {b} = {c}");
 
+            int x = 15;
+            int y = 3;
+            int z = Addition(x, y);
+            Console.WriteLine($"Différence de {x} - {y} = {z}");
+            
+        }
 
-// TODO : afficher le resultat de ces deux méthodes ici
+        // TODO : créer deux méthodes
+        public static int Addition(int premier, int second)
+        {
+            return premier + second;
+        }
 
-
-// TODO : exécuter les méthodes de test pour les deux méthodes de la classe Program.cs
+        public static int Soustraction(int premier, int second)
+        {
+            return premier - second;
+        }
+    }
+}
--- P3/P3C4/Test.cs
+++ P3/P3C4/Test.cs
@@ -3,4 +3,17 @@
 public class Test
 {
     // TODO : créer deux méthodes de test correspondant à la méthode Test en respectant la convention de nommage appropriée
+    public static void TestCinqPlusTreizeEgalDixHuit()
+    {
+        Console.WriteLine("Test de somme 5 + 13 :");
+        int testAddition = Program.Addition(5, 13);
+        Console.WriteLine(testAddition);
+    }
+    
+    public static void TestQuatorzeMoinsHuitEgalSix()
+    {
+        Console.WriteLine("Test de somme 14 - 8 :");
+        int testSoustraction = Program.Soustraction(14, 8);
+        Console.WriteLine(testSoustraction);
+    }
 }
